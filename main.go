76e1abf43373c6416cff826d076a71d9a2b20a4b
julien tellier <julien.tellier74@gmail.com>
2022-06-13 15:15:41 +0200

    get path

--- main.go
+++ main.go
@@ -38,28 +38,6 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	// users, err := queries.ListUsers(context.Background())
-	// if err != nil {
-	// 	log.Fatal("ListUsers error:", err)
-	// }
-	// fmt.Println(users)
-
-	//  erro:= queries.CreateUser(context.Background(), db.CreateUserParams{
-	// 	Name:      "fdsfs",
-	// 	Firstname: "fsdfds",
-	// 	Age:       12,
-	// })
-	// if erro != nil {
-	// 	log.Fatal("CreateUser erro:", erro)
-	// }
-	// fmt.Println(insertedUser)
-
-	// fetchedUser, err := queries.GetUser(context.Background(), insertedUser.Id)
-	// if err != nil {
-	// 	log.Fatal("GetUser error:", err)
-	// }
-	// fmt.Println(fetchedUser)
-
 	// err = queries.DeleteUser(context.Background(), insertedUser.Id)
 	// if err != nil {
 	// 	log.Fatal("DeleteUser error:", err)
